638503ee1d23b0d2ef8c055243b2528dd88b169e
Miguel Duarte <gamer-go@hotmail.com>
2018-04-13 22:54:13 +0100

    Fixed inconsistent timestamps in logfile

--- logfile.c
+++ logfile.c
@@ -6,13 +6,35 @@
 #include <unistd.h>
 
 FILE *fp = NULL;
-clock_t start;
+struct timespec start_time;
+
+struct timespec get_time_diff(struct timespec start, struct timespec end) {
+	struct timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
+double get_milis_since_start() {
+  struct timespec current_time;
+  if(clock_gettime(CLOCK_REALTIME, &current_time) != 0) {
+    fprintf(stderr, "Error in measuring current time!\n");
+    return -1;
+  }
+
+  struct timespec timediff = get_time_diff(start_time, current_time);
+
+  return (double) timediff.tv_sec * 1000.0 + (double) timediff.tv_nsec / 1000.0;
+}
 
 void writeinLog(char* text) {
   if(fp != NULL) {
-    clock_t currentTime = clock();
-    double timeFromStart = ((double) (currentTime - start)) / CLOCKS_PER_SEC;
-    fprintf(fp, "%f - %.8d - %s\n", timeFromStart, getpid(), text);
+    fprintf(fp, "%.2f - %.8d - %s\n", get_milis_since_start(), getpid(), text);
     fflush(fp);
   }
 }
@@ -30,7 +52,11 @@ int initLog() {
     return -1;
   }
 
-  start = clock();
+  if(clock_gettime(CLOCK_REALTIME, &start_time) != 0) {
+    fprintf(stderr, "Error in starting timer for logfile!\n");
+    return -2;
+  }
+
   return 0;
 }
 
